1c1c05b3ab587af90b22c483a56a169b34850299
parkmh65-hash <parkmh65@gmail.com>
2026-06-27 23:43:32 +0900

    Define State TypedDict with messages and task history

---
+++ state.py
@@ -0,0 +1,14 @@
+from typing import TypedDict, List
+from langchain_core.messages import AnyMessage
+from models import Task
+
+
+class State(TypedDict):
+
+    messages: List[AnyMessage]
+
+    task_history: List[Task]
+
+    references: dict
+
+    thread_id: str
